ea639bb0469225d6592bafa98a8e2d28df84d993
chippmann <cedric.hippmann@hotmail.com>
2024-07-29 09:37:33 +0200

    Cleanup task structure and implement copy task to copy built jars to `jvm` folder

--- kt/plugins/godot-gradle-plugin/src/main/kotlin/godot/gradle/tasks/packageBootstrapJarTask.kt
+++ kt/plugins/godot-gradle-plugin/src/main/kotlin/godot/gradle/tasks/packageBootstrapJarTask.kt
@@ -5,10 +5,7 @@ import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.tasks.TaskProvider
 
-fun Project.packageBootstrapJarTask(
-    createBuildLockTask: TaskProvider<out Task>,
-    deleteBuildLockTask: TaskProvider<out Task>
-): TaskProvider<out Task> {
+fun Project.packageBootstrapJarTask(): TaskProvider<out Task> {
     return tasks.register("packageBootstrapJar", ShadowJar::class.java) {
         with(it) {
             group = "godot-kotlin-jvm"
@@ -17,9 +14,6 @@ fun Project.packageBootstrapJarTask(
             archiveBaseName.set("godot-bootstrap")
             configurations.clear()
             configurations.add(this@packageBootstrapJarTask.configurations.getByName("bootstrap"))
-
-            dependsOn(createBuildLockTask)
-            finalizedBy(deleteBuildLockTask)
         }
     }
 }
